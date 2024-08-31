3cd0e3fa90881e89b252770fe81a2a9702be92d0
Rusta <23210116+rmukhammadaliyev@users.noreply.github.com>
2024-08-31 16:46:34 +0500

    Removed unnecessary code

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import OAuthRedirect from "./components/OAuthProvider";
 import GoogleLogin from "./components/GoogleLogin";
 import CustomGoogleLogin from "./components/CustomGoogleLogin";
 
 const MainRoutes = () => (
   <Routes>
-    <Route path="/oauth/redirect" element={<OAuthRedirect />} />
     <Route path="/custom" element={<CustomGoogleLogin />} />
     <Route path="/" element={<GoogleLogin />} />
   </Routes>
